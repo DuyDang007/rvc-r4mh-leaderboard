2a75c515bf4736ea3cf61cb7993c4693ab0b35ca
Duy Dang <duy.dang.yw@renesas.com>
2025-10-08 10:37:43 +0700

    Fix some issue, add more data

--- female.py
+++ female.py
@@ -2,10 +2,13 @@
 female_list = [
     "/athletes/132961519", # Ling Lung
     "/athletes/114163917", # mình chỉ là người bình thường
+    "/athletes/55925629",  # Bich Nguyen 
     "/athletes/125567536", # Linh LK
     "/athletes/46646019",  # Phê Nguyễn
+    "/athletes/179460826", # Phượng Lê Thị Bích
     "/athletes/132811677", # Tham Ho
     "/athletes/142018338", # Thien Ngan
     "/athletes/174567490", # Trang Lê
-    "/athletes/61004166" # Trang Pham
+    "/athletes/61004166",  # Trang Pham
+    "/athletes/133156348"  # Tu Trinh
 ]
--- groups.py
+++ groups.py
@@ -1,11 +1,33 @@
 # Map Athlete ID and group name
 groups = {
-    "TestGroup1" : [
-        "/athletes/62249220",
-        "/athletes/87907521"
+    "2465h30" : [
+        "/athletes/39488531",
+        "/athletes/151015083",
+        "/athletes/132961519",
+        "/athletes/114163917",
+        "/athletes/35632214"
         ],
-    "TestGroup2" : [
-        "/athletes/188524624",
-        "/athletes/61004166"
+    "RVC Running" : [
+        "/athletes/174567490",
+        "/athletes/188124550",
+        "/athletes/43273186",
+        "/athletes/124946603",
+        "/athletes/101046192"
+    ],
+    "DNO runners - vitamin Sea": [
+        "/athletes/109091047",
+        "/athletes/189537474",
+        "/athletes/188626879",
+        "/athletes/52067032",
+        "/athletes/189536331"
+    ],
+    "FunRun": [
+        "/athletes/56229158",
+        "/athletes/27561282",
+        "/athletes/188650521",
+        "/athletes/115533280"
+    ],
+    "lemmesolo": [
+        "/athletes/94236331"
     ]
 }
